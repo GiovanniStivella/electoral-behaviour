2ae2dc5dd4ec568ba2ab44eca34a0aa3e731d280
Giovanni Stivella <giovannistivella@MacBook-Air-di-Giovanni.local>
2025-10-02 10:37:17 -0400

    Bayesian estimation incorporating demographic covariates

--- Bayesian estimation/Demographics.R
+++ Bayesian estimation/Demographics.R
@@ -1,3 +1,4 @@
+library(lme4)
 library(tidyr)
 library(dplyr)
 library(broom)
@@ -13,7 +14,7 @@ lets <- read.csv('educ_data.csv')
 na_rows_share <- lets %>% filter(is.na(share_1))
 dem <- lets%>%
   mutate (bach = rowSums(select(lets,55:58)))%>%
-  filter(!GEOID20 %in% na_rows_prec$GEOID20)%>%
+  filter(!GEOID20 %in% na_rows_share$GEOID20)%>%
   select(1, 22:29, last_col())%>%
   unique()%>%
   filter(complete.cases(.))
@@ -25,7 +26,7 @@ g <- lets%>%
 
 #pivot wider
 h <- g %>%
-  pivot_wider(names_from = election, values_from = share_1)
+  pivot_wider(names_from = election, values_from = share_1, id_cols = GEOID20)
 
 h_clean <- h %>% drop_na() %>% filter(GEOID20 %in% dem$GEOID20) %>% select(-c(1))
 
@@ -95,31 +96,82 @@ george <- dimension$sample(
 
 summary_george <- as.data.frame(george$summary())
 
-write.csv(summary_george, "summary_0.05.csv")
+#write.csv(summary_george, "summary_demo_0.05.csv")
 
 
-#Multidimensional STAN model
+#Linear model
+educ <- lets %>%
+  filter(GEOID20 %in% dem$GEOID20) %>%
+  mutate(y = (share_1-1/2)/diff_comp)
+educ <- educ %>% 
+  mutate(bach = (per_B15003_022 + per_B15003_023 + per_B15003_024 + per_B15003_025))
 
-comp <- file.path("More complex.stan")
-work <- cmdstan_model(comp)
+ed_simple <- lmer(y ~ per_vap_hisp+per_vap_white+per_vap_black+per_vap_aian+per_vap_asian+per_vap_nhpi+per_vap_other+per_vap_two+bach+(1|GEOID20),
+                  data = educ)
 
-listfour <- list(
+ranef_ed_simple <- ranef(ed_simple)$GEOID20 %>% 
+  as.data.frame() %>% 
+  tibble::rownames_to_column("GEOID20") %>% 
+  rename(ranef_ed_simple = `(Intercept)`)
+
+fixed_effects_ed <- broom.mixed::tidy(ed_simple, effects = "fixed") %>%
+  select(term, estimate)
+
+#Linear model with first 100 precincts
+restrict <- educ[1:700, ]
+
+rest <- lmer(y ~ per_vap_hisp+per_vap_white+per_vap_black+per_vap_aian+per_vap_asian+per_vap_nhpi+per_vap_other+per_vap_two+bach+(1|GEOID20),
+             data = restrict)
+
+ranef_rest <- ranef(rest)$GEOID20 %>% 
+  as.data.frame() %>% 
+  tibble::rownames_to_column("GEOID20") %>% 
+  rename(ranef_rest = `(Intercept)`)
+
+fixed_effects_rest <- broom.mixed::tidy(rest, effects = "fixed") %>%
+  select(term, estimate)
+
+#COMPARISON
+summary_george_c <- summary_george[3:102, 2]
+scat <- cbind(ranef_rest, summary_george_c)
+
+# Scatterplot
+plot(scat$ranef_rest, scat$summary_george_c,
+     main = "Correlation of two estimates",
+     xlab = "x",
+     ylab = "y",
+     pch = 19, col = "blue")
+
+# Add correlation in title
+cor_value <- cor(scat$ranef_rest, scat$summary_george_c)
+mtext(paste("Correlation =", cor_value))
+
+
+
+
+
+
+
+#Bayesian estimation with all precincts
+listry <- list(
   T = ncol(h_clean),
   C = 12,
-  G = 100,
-  K = 2,
-  share = as.matrix(unname(n)),
+  G = nrow(h_clean),
+  M = 9,
+  share = as.matrix(unname(h_clean)),
   cand_a = as.numeric(unlist(cand_a)),
   cand_b = as.numeric(unlist(cand_b)),
-  dime = as.numeric(unlist(dime))
+  dime = as.numeric(unlist(dime)),
+  demo = as.matrix(unname(dem1))
 )
 
-ringo <- work$sample(
-  data = listfour,
+all <- dimension$sample(
+  data = listry,
   seed = 123,
   chains = 4,
   parallel_chains = 4,
   refresh = 500 # print update every 500 iters
 )
 
-summary_ringo <- as.data.frame(ringo$summary())
+
+summary_all <- as.data.frame(all$summary())
